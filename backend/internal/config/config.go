576cd2f6f0cf6a5a6342d89504b33d1e3493853f
str1xoid <134718130+str1xoid@users.noreply.github.com>
2025-03-05 17:17:42 +0300

    Config validation

--- backend/internal/config/config.go
+++ backend/internal/config/config.go
@@ -4,21 +4,23 @@ import (
 	"fmt"
 
 	"github.com/spf13/viper"
+
+	"github.com/go-playground/validator/v10"
 )
 
 type Config struct {
 	Server struct {
-		Host string `mapstructure:"host"`
-		Port string `mapstructure:"port"`
+		Host string `mapstructure:"host" validate:"required"`
+		Port string `mapstructure:"port" validate:"required"`
 	} `mapstructure:"server"`
 
 	Database struct {
-		Host     string `mapstructure:"host"`
-		Port     string `mapstructure:"port"`
-		User     string `mapstructure:"user"`
-		Password string `mapstructure:"password"`
-		DBName   string `mapstructure:"dbname"`
-		SSLMode  string `mapstructure:"sslmode"`
+		Host     string `mapstructure:"host" validate:"required"`
+		Port     string `mapstructure:"port" validate:"required"`
+		User     string `mapstructure:"user" validate:"required"`
+		Password string `mapstructure:"password" validate:"required"`
+		DBName   string `mapstructure:"dbname" validate:"required"`
+		SSLMode  string `mapstructure:"sslmode" validate:"required"`
 	} `mapstructure:"database"`
 }
 
@@ -33,6 +35,11 @@ func LoadConfig(path string) (*Config, error) {
 	var cfg Config
 	if err := viper.Unmarshal(&cfg); err != nil {
 		return nil, fmt.Errorf("unable to marshall config: %w", err)
+	}
+
+	val := validator.New()
+	if err := val.Struct(cfg); err != nil {
+		return nil, fmt.Errorf("config validation failed: %w", err)
 	}
 
 	return &cfg, nil
