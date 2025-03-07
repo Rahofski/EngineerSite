7f8f5285cf0ebea7652b41bac92c38c05df36a7f
str1xoid <134718130+str1xoid@users.noreply.github.com>
2025-03-07 19:42:23 +0300

    struct User, Request

--- backend/internal/models/request.go
+++ backend/internal/models/request.go
@@ -1,1 +1,10 @@
 package models
+
+type Request struct {
+	RequestID  int    `json:"request_id"`
+	BuildingID int    `json:"building_id"`
+	FieldID    int    `json:"field_id"`
+	UserID     int    `json:"user_id"`
+	TextInfo   string `json:"additional_text"`
+	Status     string `json:"status"`
+}
--- backend/internal/models/user.go
+++ backend/internal/models/user.go
@@ -1,1 +1,9 @@
 package models
+
+type Usesr struct {
+	UserID       int    `json:"user_id"`
+	Username     string `json:"username"`
+	PasswordHash string `json:"password_hash"`
+	Email        string `json:"email"`
+	Role         string `json:"role"`
+}
